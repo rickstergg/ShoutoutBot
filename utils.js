2ad4de64ea3ad43c1fb1fa128cf28e3800fa8e84
Rick Zhang <rickzhang@live.ca>
2021-09-25 23:22:26 -0400

    Clean up utils

--- utils.js
+++ utils.js
@@ -4,16 +4,10 @@ const vips = config.get('vips');
 
 const greetVIP = (username, displayName) => `HI BEEB!`;
 const greetStreamer = (username, displayName) => `!so ${displayName}`;
-
 const isVIP = username => vips.includes(username);
 const isStreamer = username => streamers.includes(username);
 const formatLeagueRank = ({ tier, rank, leaguePoints, hotStreak }) => `${tier} ${rank} - ${leaguePoints} LP ${hotStreak ? '🔥' : ''}`;
-
-const getCooldownPercentage = cooldown => {
-  const cd = parseFloat(cooldown);
-  return (1 - (1 / (1 + (cooldown / 100)))) * 100;
-};
-
+const getCooldownPercentage = cooldown => (1 - (1 / (1 + (cooldown / 100)))) * 100;
 const getVersionsUrl = () => 'https://ddragon.leagueoflegends.com/api/versions.json';
 const getChampUrl = (version, champ) => `http://ddragon.leagueoflegends.com/cdn/${version}/data/en_US/champion/${champ}.json`;
 
